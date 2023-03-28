f44f24f7bd6ed7f03073b5afa6cfa605dac87555
ChrisWalker264 <101838158+ChrisWalker264@users.noreply.github.com>
2023-03-27 20:33:49 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+const ITEMS_CONTAINER = document.getElementID("items");
+const ITEMS_TEMPLATE = document.getElementID("itemsTemplate");
+const ADD_BUTTON = document.getElementID("add");
+
+let items = getItems();
+
+function getItems() {
+    const value = localStorage.getItem("todo-test") || "[]";
+
+    return JSON.parse(value);
+}
+
+function setItems(items) {
+    const itemsJson = JSON.stringify(items);
+
+    localStorage.setItem("todo-test", itemsJSON);
+}
+
+function addItem() {
+    items.unshift({
+        description: "",
+        completed: false
+    });
+
+    setItems(items);
+    refreshList();
+}
+
+function updateItem(item, key, value) {
+    item[key] = value;
+    setItems(items);
+    refreshList();
+}
+
+function refreshList() {
+    items.sort((a, b) => {
+        if (a.completed) {
+            return 1;
+        }
+
+        if (b.completed) {
+            return -1
+        }
+
+        return a.description < b.description ? -1 : 1;
+    })
+    //TODO: sort items
+    
+    ITEMS_CONTAINER.innerHTML = "";
+    for (const item of items) {
+        const itemElement = ITEMS_TEMPLATE.content.cloneNODE(true);
+        const descriptionInput = itemElement.querySelector(".item-description");
+        const completedInput = itemElement.querySelector(".item-completed");
+
+        descriptionInput.value = item.description;
+        completedInput.checked = item.completed;
+
+        ITEMS_CONTAINER.append(itemElement);
+    }
+    descriptionInput.addEventListener("change", () => {
+        updateItem(item, "description", descriptionInput.value);
+
+    completedInput.addEventListener("change", () => {
+        updateItem(item, "completed", completedInput.checked);
+        });
+    }
+}
+
+
+
+ADD_BUTTON.adEventListener("click", () => {
+    addItem();
+})
+
+refreshList();
+
+// video https://www.youtube.com/watch?v=cijPd-TXPn4&t=102s
